25e60f605ef9061b6c26e5ed55cd61c8d1042b18
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-09-19 12:05:05 +0300

    Выполнено 1 домашнее задание
    1.Установить vue-cli
    2.Создать новое приложение vue@2.x c использование vue-cli
    3.Повторить код калькулятора
    4.Также в качестве домашнего задания мы предлагаем расширить возможности нашего калькулятора который мы написали на уроке. Добавьте кнопки и возможность выполнения следующих действий:
    5.Возведение в степень
    6.Целочисленное деление

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    {{ message }}
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" placeholder="text" @blur="blurMethod" />
+    <button @click="c = a + b">Click</button>
+    <button @click="doThat">Clear</button>
+    {{ c }}
+    <Calc />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Calc from "./components/Calc.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Calc
+  },
+  data: () => ({
+    message: "Привет, VUE",
+    c: 0,
+    a: 1,
+    b: 1
+  }),
+  methods: {
+    doThat() {
+      this.c = 0;
+    },
+    blurMethod() {
+      console.log("blur");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
