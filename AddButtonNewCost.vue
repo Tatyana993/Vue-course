53b9c82121acdcaa2f462952530ad5a9636980ce
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-10-06 00:17:18 +0300

    Add files via upload

---
+++ AddButtonNewCost.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="addNewCost">ADD NEW COST+</button>
+</template>
+
+<script>
+export default {
+  name: AddButtonNewCost,
+  data() {
+    return {},
+     methods addNewCost: {
+
+     }
+  },
+};
+</script>
+
+<style></style>
