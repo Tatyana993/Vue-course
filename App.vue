9812a73f9e97a511218aa12244db518495ac1b55
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-10-28 14:13:04 +0300

    DZ-6
    Создано контекстное меню, в контекстном меню добавлены 2 пункта: редактирование траты и удаление.

--- App.vue
+++ App.vue
@@ -11,20 +11,25 @@
     <transition name="fade">
       <modal-window-add-payment v-if="modalIsShow" :settings="modalSettings" />
     </transition>
+    <transition name="fade2">
+      <edit-window v-if="editIsShow" :settings2="editSettings" />
+    </transition>
   </div>
 </template>
 
 <script>
-//import ModalWindowAddPayment from "./components/ModalWindowAddPayment.vue";
 export default {
   name: "App",
   components: {
     ModalWindowAddPayment: () =>
       import("./components/ModalWindowAddPayment.vue"),
+    EditWindow: () => import("./components/EditWindow.vue"),
   },
   data: () => ({
     modalSettings: {},
     modalIsShow: false,
+    editSettings: {},
+    editIsShow: false,
   }),
   computed: {},
   methods: {
@@ -56,10 +61,19 @@ export default {
       this.modalSettings = {};
       this.modalIsShow = false;
     },
+    shownEdit(settings2) {
+      this.editIsShow = true;
+      this.editSettings = settings2;
+    },
+    hideEdit() {
+      this.editIsShow = false;
+    },
   },
   mounted() {
     this.$modal.EventBus.$on("onShown,", this.onShown);
     this.$modal.EventBus.$on("onHide,", this.onHide);
+    this.$edit.EventBus.$on("shownEdit,", this.shownEdit);
+    this.$edit.EventBus.$on("hideEdit,", this.hideEdit);
   },
 
   created() {
@@ -92,6 +106,14 @@ export default {
   a {
     padding-left: 10px;
     padding-bottom: 10px;
+  }
+  .fade2-enter-active,
+  .fade2-leave-active {
+    transition: opacity 0.3s;
+  }
+  .fade2-enter,
+  .fade2-leave-to {
+    opacity: 0;
   }
 }
 </style>
--- main.js
+++ main.js
@@ -3,13 +3,16 @@ import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import modal from "./plugins/modalWindows";
+import edit from "./plugins/EditWindowOpen";
 
 Vue.config.productionTip = false;
 Vue.use(modal);
+Vue.use(edit);
 
 new Vue({
   render: (h) => h(App),
   store,
   router,
   modal,
+  edit,
 }).$mount("#app");
